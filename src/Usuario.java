ab3760e200a87ef02bd4650ad73ce490ee5683ad
Daniel Gonzalez <dlgt1606@gmail.com>
2025-08-24 08:23:15 -0500

    demo 1

---
+++ src/Usuario.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sergi
+ */
+    public class Usuario {
+
+        private String email;
+        private String password;
+
+        public Usuario(String email, String password) {
+            this.email = email;
+            this.password = password;
+        }
+
+        // Getters y setters si los necesitas
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
